49da366866b43aad7f8cc2e96bc725ab071e95c3
sayam9901 <sayamkhan9560621912@gmail.com>
2023-09-22 18:58:00 +0530

    portal ocncept

--- src/components/Warning.js
+++ src/components/Warning.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import ReactDOM from 'react-dom';
+const Model = () =>{
+    return  <div>Please fill both the field</div>
+}
 
 const Warning = () => {
   return (
-    <div>Please fill both the field</div>
+   <>
+ { ReactDOM.createPortal(<Model/>,document.getElementById("portal-root"))}
+   </>
   )
 }
 
